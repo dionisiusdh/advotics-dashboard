403e86e8fd193564b63639c23a2ef7f80a5060b4
Dionisius Darryl Hermansyah <dionisius.dh28@gmail.com>
2021-03-20 20:49:17 +0800

    Sidebar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 
 import Navbar from './navigation/Navbar';
+import Sidebar from './navigation/Sidebar';
 import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Dashboard />
+      <div className="Body">
+        <Sidebar />
+        <Dashboard />
+      </div>
     </div>
   );
 }
--- src/navigation/Sidebar.js
+++ src/navigation/Sidebar.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+
+import './Sidebar.css';
+
+import menu from '../assets/icon-menu.jpg';
+import icon from '../assets/icon-analysis.svg';
 
 export default function Sidebar() {
     return (
-        <div>
-            
+        <div className='side'>
+            <div clsasName='side-menu'>
+                <img className='side-img' src={menu} alt='menu'/>
+            </div>
+            <div className='side-icon'>
+                <img className='side-img2' src={icon} alt='icon'/>
+            </div>
         </div>
     )
 }
